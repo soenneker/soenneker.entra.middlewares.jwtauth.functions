af26e7c36fade83143550e0f2c186f4127d5ce88
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:43 -0500

    TUnit conversion

--- test/Soenneker.Entra.Middlewares.JwtAuth.Functions.Tests/JwtAuthMiddlewareTests.cs
+++ test/Soenneker.Entra.Middlewares.JwtAuth.Functions.Tests/JwtAuthMiddlewareTests.cs
@@ -1,16 +1,15 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Entra.Middlewares.JwtAuth.Functions.Tests;
 
-[Collection("Collection")]
-public sealed class JwtAuthMiddlewareTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class JwtAuthMiddlewareTests : HostedUnitTest
 {
-    public JwtAuthMiddlewareTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public JwtAuthMiddlewareTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
     }
